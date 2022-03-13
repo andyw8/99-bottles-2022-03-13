245ff78bc7418e824061342a5d7eb3e6cd0f4188
Andy Waite <andyw8@users.noreply.github.com>
2022-03-13 09:57:59 -0400

    WIP

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
         "Take it down and pass it around, " + "no more bottles of beer on the wall.\n"
     when 2
       "#{number} bottles of beer on the wall, " + "#{number} bottles of beer.\n" +
-        "Take one down and pass it around, " + "1 bottle of beer on the wall.\n"
+        "Take one down and pass it around, " + "#{number - 1} bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " + "#{number} bottles of beer.\n" +
         "Take one down and pass it around, " + "#{number - 1} bottles of beer on the wall.\n"
