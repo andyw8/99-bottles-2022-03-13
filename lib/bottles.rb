c56afcaabf09c71066a7067b625548859c4f194c
Andy Waite <andyw8@users.noreply.github.com>
2022-03-13 10:01:01 -0400

    WIP

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -17,15 +17,19 @@ class Bottles
         "#{number} bottle of beer.\n" +
         "Take it down and pass it around, " + "no more bottles of beer on the wall.\n"
     when 2
-      "#{number} bottles of beer on the wall, " + "#{number} bottles of beer.\n" +
-        "Take one down and pass it around, " + "#{number - 1} bottle of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, " +
+        "#{number} bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "#{number - 1} bottle of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " + "#{number} bottles of beer.\n" +
-        "Take one down and pass it around, " + "#{number - 1} #{container} of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, " +
+        "#{number} bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "#{number - 1} #{container} of beer on the wall.\n"
     end
   end
 
-  def container
+  def container(number = :FIXME)
     "bottles"
   end
 end
